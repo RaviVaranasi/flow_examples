dd1d62f3d5ea374031f9284de4e4ba808b6908eb
Ravi Varanasi <r_varanasi@ISDV160954L.local>
2015-07-06 22:32:32 -0500

    Initial working of flow/es6 example-2

--- src/example1.js
+++ src/example1.js
@@ -1,4 +1,6 @@
 /* @flow */
 var hello_world = (name: string) => "hello world " + name;
 
-console.log(hello_world('name'));
+console.log(hello_world('bob'));
+
+console.log(hello_world({name: 'bob'}));
